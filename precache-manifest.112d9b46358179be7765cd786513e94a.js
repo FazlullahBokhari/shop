4b7b01d829a5b689c63ff75cd1896db97e18e5b5
fazlullahb0 <fazlullahb0@gmail.com>
2020-06-08 16:55:36 +0530

    Updates

---
+++ precache-manifest.112d9b46358179be7765cd786513e94a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c868b424dfe0311f5cc28e68b91230c9",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "8db6f3f2ad7d5acebdd8",
+    "url": "/shop/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5875bf21496e43f9cd83",
+    "url": "/shop/static/js/2.3c0dd919.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop/static/js/2.3c0dd919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db6f3f2ad7d5acebdd8",
+    "url": "/shop/static/js/main.daf76265.chunk.js"
+  },
+  {
+    "revision": "e15625989d71d6fe1728",
+    "url": "/shop/static/js/runtime-main.e68cc5c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
